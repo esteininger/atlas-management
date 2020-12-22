845ac00440d42e40ede2fbfff1fd96509d5b2111
Ethan Steininger <esteininger21@gmail.com>
2020-12-22 09:41:31 -0500

    added deleteCluster SH script

---
+++ cluster_management/deleteCluster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. ../env.config
+CLUSTER='sample'
+
+curl --user "${PUBLICKEY}:${PRIVATEKEY}" --digest \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --include \
+  --request DELETE "https://cloud.mongodb.com/api/atlas/v1.0/groups/${GROUPID}/clusters/${CLUSTER}?pretty=true" \
