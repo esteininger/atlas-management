f0508abc9ba5a733c2614419c575f981f1c4f0c6
Ethan Steininger <esteininger21@gmail.com>
2020-12-22 09:49:30 -0500

    add whitelist

---
+++ security/whitelistIP.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. ../env.config
+
+curl --user "${PUBLICKEY}:${PRIVATEKEY}" --digest \
+--header "Accept: application/json" \
+--header "Content-Type: application/json" \
+--include \
+--request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/${GROUPID}/accessList?pretty=true" \
+--data-raw '
+  [
+    {
+      "ipAddress" : "0.0.0.0",
+      "comment" : "Allow all IP addresses for server"
+    }
+  ]
+'
