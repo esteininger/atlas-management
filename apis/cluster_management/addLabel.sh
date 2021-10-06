b8399511416ea628d4ac92a3cedcb55cc50bfbf1
esteininger <esteininger21@gmail.com>
2021-10-06 14:30:00 -0700

    change folder structure

---
+++ apis/cluster_management/addLabel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+. ../env.config
+CLUSTER='FreddieMacDev'
+
+curl --user "${PUBLICKEY}:${PRIVATEKEY}" --digest \
+--header "Accept: application/json" \
+--header "Content-Type: application/json" \
+--include \
+--request PATCH "https://cloud.mongodb.com/api/atlas/v1.0/groups/${GROUPID}/clusters/${CLUSTER}?pretty=true" \
+--data '
+  {"labels": [{"key":"mDeleter", "value":"{2022-01-01}"}]}
+'
